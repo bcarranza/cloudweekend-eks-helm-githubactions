5efb2a6d1ff524b1ce0a5de276d6bed4b35e4a58
bcarranza <bayron.carranza@gmail.com>
2024-03-03 20:48:01 -0600

    mx

--- code/app.py
+++ code/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello World from CAN</h2>'
+    return '<h1>Hello World from mx</h2>'
 
 if __name__ == "__main__":
     app.run(debug=True)
