f2c510c5e9398d85426c0bfd694ef92e6eb657fb
bcarranza <bayron.carranza@gmail.com>
2024-03-03 19:31:33 -0600

    fix script

--- eksctl/create_cluster.sh
+++ eksctl/create_cluster.sh
@@ -1,3 +1,5 @@
-eksctl create cluster - name k8s-UAEH \
- - region us-east-1 \
- - node-type t3.micro
+eksctl create cluster --name k8s-UAEH \
+ --region us-east-1 \
+ --node-type t3.micro \
+ --nodes 3 \
+ --version 1.29
