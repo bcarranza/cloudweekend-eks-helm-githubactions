71c7e565d8296fd7fcf61772b2c1ce134bc0eb59
Bayron Carranza <bayron.carranza@gmail.com>
2024-03-08 22:32:32 -0600

    Update create_cluster.sh

--- eksctl/create_cluster.sh
+++ eksctl/create_cluster.sh
@@ -1,5 +1,7 @@
-eksctl create cluster --name k8s-UAEH \
- --region us-east-1 \
- --node-type t3.micro \
- --nodes 3 \
- --version 1.29
+# Command to create an Amazon EKS cluster using eksctl
+eksctl create cluster \
+    --name k8s-UAEH \                          # Specify the name of the EKS cluster
+    --region us-east-1 \                        # Specify the AWS region where the cluster will be created
+    --node-type t3.micro \                      # Specify the EC2 instance type for worker nodes
+    --nodes 3 \                                 # Specify the number of worker nodes to be created
+    --version 1.29                              # Specify the Kubernetes version for the cluster
