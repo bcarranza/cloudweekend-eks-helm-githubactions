96993324e6862bbcd5be418e1e13ffaeb501b0ae
Bayron Carranza <bayron.carranza@gmail.com>
2024-03-08 22:40:00 -0600

    Update variables.tf

--- terraform/iam-base/variables.tf
+++ terraform/iam-base/variables.tf
@@ -1,32 +1,33 @@
+# Variable for specifying the name for the various resources
 variable "name" {
-  description = "The name for the various resources"
-  default     = "github_oidc"
+  description = "The name for the various resources"  # Description of the variable
+  default     = "github_oidc"                         # Default value for the variable
 }
 
+# Variable for specifying the URL of the identity provider
 variable "url" {
-  default = "https://token.actions.githubusercontent.com"
+  default = "https://token.actions.githubusercontent.com"  # Default value for the variable
 }
 
+# Variable for specifying the list of client IDs for the identity provider
 variable "client_id_list" {
-  type = list(string)
-  default = [
-    "sts.amazonaws.com"
-  ]
+  type    = list(string)                     # Type constraint for the variable (list of strings)
+  default = ["sts.amazonaws.com"]            # Default value for the variable
 }
 
-# This is the thumbprint returned if you were to create an "identity provider" in AWS and gave
-# it this url: https://token.actions.githubusercontent.com
+# Variable for specifying the list of thumbprints of the provider's server certificates
 variable "thumbprint_list" {
-  type = list(string)
-  default = [
+  type    = list(string)                     # Type constraint for the variable (list of strings)
+  default = [                                # Default value for the variable
     "6938fd4d98bab03faadb97b34396831e3780aea1",
     "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
   ]
 }
 
+# Variable for specifying the AWS policy in JSON format
 variable "aws_policy_json" {
-  description = "The AWS policy in a json format"
-  default     = <<-EOT
+  description = "The AWS policy in a json format"  # Description of the variable
+  default     = <<-EOT                             # Default value for the variable
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -40,20 +41,23 @@ variable "aws_policy_json" {
 EOT
 }
 
+# Variable for specifying the conditions to validate
 variable "validate_conditions" {
-  description = "Conditions to validate"
-  type        = set(string)
-  default     = ["repo:bcarranza/cloudweekend-eks-helm-githubactions:ref:refs/heads/main"]
+  description = "Conditions to validate"    # Description of the variable
+  type        = set(string)                 # Type constraint for the variable (set of strings)
+  default     = ["repo:bcarranza/cloudweekend-eks-helm-githubactions:ref:refs/heads/main"]  # Default value for the variable
 }
 
+# Variable for specifying tags to assign to resources
 variable "tags" {
-  type        = map(any)
-  default     = {}
-  description = "Tags"
+  type        = map(any)                    # Type constraint for the variable (map of any type)
+  default     = {}                          # Default value for the variable
+  description = "Tags"                       # Description of the variable
 }
 
+# Variable for controlling whether to create the identity provider or not
 variable "create_identity_provider" {
-  type        = bool
-  default     = true
-  description = "This switch allows you to create or not create the identity provider.  Only one can exist.  If you are creating multiple Github OIDC Federations, only one of the instantiations should create this or the Terraform run will fail."
+  type        = bool                        # Type constraint for the variable (boolean)
+  default     = true                        # Default value for the variable
+  description = "This switch allows you to create or not create the identity provider. Only one can exist. If you are creating multiple Github OIDC Federations, only one of the instantiations should create this or the Terraform run will fail."  # Description of the variable
 }
