ec55cfe274249a3d4509276a04d8698356fe52f7
Bayron Carranza <bayron.carranza@gmail.com>
2024-03-08 22:35:44 -0600

    Update output.tf

--- terraform/iam-base/output.tf
+++ terraform/iam-base/output.tf
@@ -1,3 +1,4 @@
+# Output block for exposing the ARN of the IAM role created by the iam_assumable_role_admin module
 output "arn" {
-  value = module.iam_assumable_role_admin.this_iam_role_arn
+  value = module.iam_assumable_role_admin.this_iam_role_arn  # Outputting the ARN of the IAM role
 }
