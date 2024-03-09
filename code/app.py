e27fcec388a956b12cd9ed7b3ebd631ba3a8e068
Bayron Carranza <bayron.carranza@gmail.com>
2024-03-08 22:30:14 -0600

    app code

--- code/app.py
+++ code/app.py
@@ -1,9 +1,16 @@
+# Import the Flask class from the flask package
 from flask import Flask
+
+# Create an instance of the Flask class with the name of the current module
 app = Flask(__name__)
 
+# Define a route for the root URL '/'
 @app.route('/')
 def hello_world():
+    # Define the view function that returns the response for the route
     return '<h1>Hello World from GT</h2>'
 
+# Ensure that the Flask application is run only if the script is executed directly
 if __name__ == "__main__":
+    # Run the Flask application in debug mode, enabling debug messages
     app.run(debug=True)
