929817d21269e71a2a3aeb089e87992fcf3a35e4
bcarranza <bayron.carranza@gmail.com>
2024-03-08 23:52:29 -0600

    remove some typos.

--- eksctl/remove_cluster.sh
+++ eksctl/remove_cluster.sh
@@ -1,5 +1,5 @@
 # Command to delete an Amazon EKS cluster using eksctl
 eksctl delete cluster \
-    --name k8s-UAEH \                          # Specify the name of the EKS cluster to be deleted
-    --region us-east-1                         # Specify the AWS region where the cluster is located
+    --name k8s-UAEH \
+    --region us-east-1
 
